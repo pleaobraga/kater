0c9e99928c49c55f78e822a9c2f726c2014bb4ed
Pedro Leâo <pleao.braga@gmail.com>
2019-01-30 02:56:17 -0200

    add seo validadtion

--- src/components/CardPlains.jsx
+++ src/components/CardPlains.jsx
@@ -9,7 +9,7 @@ const CardPlains = ({card, selectPlain, form, plain}) => {
         <CardBase card={card} >
             <PlainsList card={card} selectPlain={selectPlain} />
             {
-                 Object.entries(plain).length > 0 && (
+                 plain.type !== undefined && (
                     <RegisterPlainForm form={form} plain={plain} />
                 )
             }
