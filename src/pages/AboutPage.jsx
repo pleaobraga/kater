d0f12db4550d6b34382596a1c8eeb7f840d6cbe7
Pedro Braga <plbraga@mmsi.com>
2019-01-30 10:35:41 -0200

    created link to contract for all pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import { SecondaryBanner, CardCTA } from '../components'
 import { about } from '../data/about.json';
+import  ContractPage  from './ContractPage';
 
 
-const AboutPage = () => {
-  return (
-      <section className="section section-about about">
-        <SecondaryBanner  banner={about} />
-        <CardCTA card={about.card} />      
-      </section>
-  );
+class AboutPage extends React.Component {
+
+  constructor() {
+    super()
+    this.state = { 
+      width: window.innerWidth
+    };
+  }
+
+  updateDimensions = () => {
+    this.setState({
+      width: window.innerWidth
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+
+  render() {
+    const { width } = this.state
+
+    return (
+      <React.Fragment>
+        <section className="section section-about about">
+          <SecondaryBanner  banner={about} />
+          <CardCTA card={about.card} />             
+        </section>
+        {
+          width < 768 && ( <ContractPage classesName={"fix-top"} /> )
+        }
+      </React.Fragment>
+    );
+  }
 };
 
 export default AboutPage;
--- src/pages/ContractPage.jsx
+++ src/pages/ContractPage.jsx
@@ -3,13 +3,17 @@ import { SecondaryBanner, CardCityList } from '../components'
 import { contract } from '../data/contract.json';
 
 
-const ContractPage = () => {
+const ContractPage = ({classesName}) => {
   return (
-    <section className="section section-where where">
+    <section className={`section section-where where ${classesName}`}>
        <SecondaryBanner  banner={contract} />
         <CardCityList card={contract.card} />     
     </section>
   );
 };
+
+ContractPage.defaultProps = {
+  classesName: ""
+}
 
 export default ContractPage;
